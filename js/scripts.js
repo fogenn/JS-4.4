5db880a67d02700d2b43b4563e9431248a6bce9e
fogenn <fogennnn@gmail.com>
2020-04-04 16:42:58 +0500

    page layout complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-$(function(){
-  
+$(document).ready(function(){
+
+  function addTask() {
+
+    let name = $('#created-tasks__input').val(),
+      text = $('#created-tasks__textarea').val(),
+
+    if (name.length !== 0 && text.length !== 0) {
+      
+    }
+  }
+
 })
